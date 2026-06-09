eec982ec9ac6189fbf0496b1b076eb1ca390d74a
vidhi070518 <vidhi070518@gmail.com>
2026-06-09 14:51:44 +0530

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://map-future.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://map-future.vercel.app/careers/data-analyst',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://map-future.vercel.app/careers/ai-engineer',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://map-future.vercel.app/careers/cybersecurity-analyst',
+      lastModified: new Date(),
+    },
+  ];
+}
