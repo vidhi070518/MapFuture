ecef7a5049f98839ad55cc348f0d2aa8e7d5c5af
vidhi070518 <vidhi070518@gmail.com>
2026-06-10 14:17:11 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,9 +1,11 @@
-export default function robots() {
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
     sitemap: 'https://map-future.vercel.app/sitemap.xml',
-  };
+  }
 }
